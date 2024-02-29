3918d5a5ad76b3c6a59f549a82735c2ea0f77d65
abderrahman.youabd <youabd50@gmail.com>
2024-02-29 10:31:44 +0100

    refactor: use Lombok in Nationality

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -9,23 +9,16 @@ import java.util.List;
 @Data
 
 public class Nationality {
-
     private long count;
     private String name;
-
-
-
     @SerializedName("country")
     private List<Country> countries;
 
 
     @Data
-
     public static class Country {
-
         private String countryId;
         private float probability;
-
 
     }
 
